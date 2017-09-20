cebcab0fcb09922ed4512a1df7895fccc5ed8536
Ciro Cesar Maciel <ciro.maciel@c37.co>
2017-09-19 22:15:20 -0300

    - string - toHex

--- index.js
+++ index.js
@@ -210,6 +210,16 @@ var utility = {
             }
         }
     })(),
+    string: {
+        // https://stackoverflow.com/questions/36637146/javascript-encode-string-to-hex
+        toHex: function (tmp) {
+            var str = '';
+            for (var i = 0; i < tmp.length; i++) {
+                str += tmp[i].charCodeAt(0).toString(16);
+            }
+            return str;
+        }
+    }
     // crypto: {
     //     hash: function hash(pwd, salt, fn) {
 
