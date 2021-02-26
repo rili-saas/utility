12d1489dc23286be27860563a99124c271ba50e9
Ciro Cesar Maciel <me@ciro-maciel.me>
2021-02-26 18:51:40 -0300

    refactor: directory rename

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export { default as backEnd } from "./back-end";
-export { default as frontEnd } from "./front-end";
+export { default as backEnd } from "./backend";
+export { default as frontEnd } from "./frontend";
 export { default as utility } from "./utility";
