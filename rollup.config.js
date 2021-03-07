937065baa99662fb949c0fdd4baf677c9348f224
Ciro Cesar Maciel <me@ciro-maciel.me>
2021-03-07 16:54:10 -0300

    fix: dependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,7 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-// import json from "@rollup/plugin-json";
-// import externalGlobals from "rollup-plugin-external-globals";
 
 export default {
-  plugins: [
-    nodeResolve(),
-    // json(),
-    // externalGlobals({
-    //   "aws-sdk": "aws-sdk",
-    //   react: "react",
-    // }),
-  ],
+  plugins: [nodeResolve()],
   input: "src/index.js",
   external: ["aws-sdk", "react"],
   output: [
