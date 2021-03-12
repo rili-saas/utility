5b94cf4539fd554911a68b528007f7fbfb8275dd
Ciro Cesar Maciel <me@ciro-maciel.me>
2021-03-12 07:30:08 -0300

    fix: useNetInfo

--- src/frontend/hooks/index.js
+++ src/frontend/hooks/index.js
@@ -1,6 +1,5 @@
 // https://pt-br.reactjs.org/docs/hooks-custom.html
-import useNetInfo from "./use-net-info";
 import useInfiniteScroll from "./use-infinite-scroll";
 import useUpdate from "./use-update";
 
-export default { useUpdate, useInfiniteScroll, useNetInfo };
+export default { useUpdate, useInfiniteScroll };
