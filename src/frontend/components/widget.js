f4522a684be5d560b01c0100634516af6d4723b1
Ciro Cesar Maciel <me@ciro-maciel.me>
2021-03-25 10:47:42 -0300

    fix(widget): style

--- src/frontend/components/widget.js
+++ src/frontend/components/widget.js
@@ -117,7 +117,10 @@ HeaderContent.Option = HeaderOption;
 function BodyContent(props) {
   const { children, style } = props;
   return (
-    <div {...props} style={{ flex: "1", padding: "16px", ...style }}>
+    <div
+      {...props}
+      style={{ flex: "1", padding: "8px 16px 16px 16px", ...style }}
+    >
       {children}
     </div>
   );
