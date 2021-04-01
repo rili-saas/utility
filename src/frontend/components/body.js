44f7b562b7b61d8dd2e5282859189937b8870761
Ciro Cesar Maciel <me@ciro-maciel.me>
2021-04-01 11:14:28 -0300

    fix(body): add size parameter

--- src/frontend/components/body.js
+++ src/frontend/components/body.js
@@ -4,12 +4,12 @@ import PropTypes from "prop-types";
 import { Row } from "antd";
 
 const Body = (props) => {
-  const { children, style, ...rest } = props;
+  const { children, style, size = 144, ...rest } = props;
   return (
     <Row
       justify="center"
       style={{
-        minHeight: "calc(calc(100vh - calc(100vh - 100%)) - 144px)",
+        minHeight: `calc(calc(100vh - calc(100vh - 100%)) - ${size}px)`,
         width: "100%",
         padding: "0 8px",
       }}
@@ -31,8 +31,9 @@ const Body = (props) => {
 };
 
 Body.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  children: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
   style: PropTypes.object,
+  size: PropTypes.number.isRequired,
 };
 
 export default Body;
