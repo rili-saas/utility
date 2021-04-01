ba8af32feb100a6d467009d4210005e193969262
Ciro Cesar Maciel <me@ciro-maciel.me>
2021-04-01 11:27:27 -0300

    fix(body): size to object

--- src/frontend/components/body.js
+++ src/frontend/components/body.js
@@ -4,12 +4,12 @@ import PropTypes from "prop-types";
 import { Row } from "antd";
 
 const Body = (props) => {
-  const { children, style, size = 144, ...rest } = props;
+  const { children, style, size = { bottom: 144 }, ...rest } = props;
   return (
     <Row
       justify="center"
       style={{
-        minHeight: `calc(calc(100vh - calc(100vh - 100%)) - ${size}px)`,
+        minHeight: `calc(calc(100vh - calc(100vh - 100%)) - ${size.bottom}px)`,
         width: "100%",
         padding: "0 8px",
       }}
@@ -33,7 +33,7 @@ const Body = (props) => {
 Body.propTypes = {
   children: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
   style: PropTypes.object,
-  size: PropTypes.number.isRequired,
+  size: PropTypes.object.isRequired,
 };
 
 export default Body;
